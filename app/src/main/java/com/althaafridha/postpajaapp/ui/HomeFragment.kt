5d3c28e4475ad9158c2219d8ad1f57ab50d72d44
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2021-12-04 11:08:05 +0700

    added toast to home page

--- app/src/main/java/com/althaafridha/postpajaapp/ui/HomeFragment.kt
+++ app/src/main/java/com/althaafridha/postpajaapp/ui/HomeFragment.kt
@@ -2,12 +2,14 @@ package com.althaafridha.postpajaapp.ui
 
 import android.app.Activity
 import android.content.Context
+import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.althaafridha.postpajaapp.R
@@ -34,5 +36,19 @@ class HomeFragment : Fragment() {
 
         }
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val btnUpload: ImageButton = view.findViewById(R.id.btn_upload)
+        val btnMessage: ImageButton = view.findViewById(R.id.btn_message)
+
+        btnUpload.setOnClickListener {
+            Toast.makeText(activity,"Upload Is Not Available",Toast.LENGTH_SHORT).show();
+        }
+
+        btnMessage.setOnClickListener {
+            Toast.makeText(activity,"Message Is Not Available",Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/com/althaafridha/postpajaapp/ui/ProfileFragment.kt
+++ app/src/main/java/com/althaafridha/postpajaapp/ui/ProfileFragment.kt
@@ -1,34 +1,19 @@
 package com.althaafridha.postpajaapp.ui
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.content.ContextCompat
+import com.althaafridha.postpajaapp.PostActivity
+import com.althaafridha.postpajaapp.PostDuactivity
 import com.althaafridha.postpajaapp.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ProfileFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ProfileFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,23 +23,20 @@ class ProfileFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ProfileFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ProfileFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val post1: ImageView = view.findViewById(R.id.post_1)
+        val post2: ImageView = view.findViewById(R.id.post_2)
+
+        post1.setOnClickListener {
+            val intent = Intent(activity, PostActivity::class.java)
+            startActivity(intent)
+        }
+
+        post2.setOnClickListener {
+            val intent = Intent(activity, PostDuactivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
